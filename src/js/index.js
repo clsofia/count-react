e16de283c66a83da9b2562c93acc1ef90cbdb533
Cláudia Diogo <138239815+clsofia@users.noreply.github.com>
2023-08-31 09:43:53 +0000

    exercise done

--- src/js/index.js
+++ src/js/index.js
@@ -14,12 +14,12 @@ function SimpleCounter(props) {
         <div className="calendar">
             <i className="far fa-clock"></i>
         </div>
-        <div className="six">{props.digitSix %10}</div>
-        <div className="five">{props.digitFive %10}</div>
-        <div className="four">{props.digitFour %10}</div>
-        <div className="three">{props.digitThree %10}</div>
-        <div className="two">{props.digitTwo %10}</div>
-        <div className= "one">{props.digitOne %10}</div>
+        <div className="six">{props.digitSix % 10}</div>
+        <div className="five">{props.digitFive % 10}</div>
+        <div className="four">{props.digitFour % 10}</div>
+        <div className="three">{props.digitThree % 10}</div>
+        <div className="two">{props.digitTwo % 10}</div>
+        <div className="one">{props.digitOne % 10}</div>
 
     </div>);
 
@@ -34,33 +34,33 @@ SimpleCounter.propTypes = {
     digitThree: PropTypes.number,
     digitTwo: PropTypes.number,
     digitOne: PropTypes.number,
-    
+
 
 };
 
 let counter = 0;
 
-setInterval(function() {
+setInterval(function () {
 
-    const six = Math.floor(counter/1000000);
-    
-    const five = Math.floor(counter/10000);
-    
-    const four = Math.floor(counter/1000);
-    
-    const three = Math.floor(counter/100);
-    
-    const two = Math.floor(counter/10);
-    
-    const one = Math.floor(counter/1);
+    const six = Math.floor(counter / 1000000);
+
+    const five = Math.floor(counter / 10000);
+
+    const four = Math.floor(counter / 1000);
+
+    const three = Math.floor(counter / 100);
+
+    const two = Math.floor(counter / 10);
+
+    const one = Math.floor(counter / 1);
 
     console.log(six, five, four, three, two, one);
 
-counter ++;
+    counter++;
 
 
-//render your react application
-ReactDOM.render(<SimpleCounter digitOne={one} digitTwo = {two}  digitThree={three}  digitFour={four} digitFive={five} digitSix={six}/>, 
-document.querySelector("#app")
-);
-},1000);
+    //render your react application
+    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />,
+        document.querySelector("#app")
+    );
+}, 1000);
